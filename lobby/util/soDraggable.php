35556c777b707c535e5fa4821233b1cb93c74088
Andrew Brainerd <abrainerd@umcu.org>
2016-10-05 13:52:35 -0400

    Still playing with drag/drop functionality

--- lobby/util/soDraggable.php
+++ lobby/util/soDraggable.php
@@ -11,49 +11,61 @@
 <html>
 <head>
     <title>jQuery Drag/Drop Test</title>
+    <link rel="stylesheet" href="/lobby/css/lobby.css"/>
     <style>
         body {
             background: #00274c;
             font-family: "Lucida Sans", sans-serif;
         }
-
         #visitor {
             position: absolute;
         }
 
+        #container {
+            display: inline-block;
+            width: 500px;
+        }
+
+        #viewVisitors {
+            display: inline-block;
+            font-size: 14px;
+            margin-top: 10px;
+            padding: 10px;
+            vertical-align: top;
+        }
+
+        #viewVisitors .table {
+            font-size: 12px;
+            max-width: none;
+        }
         .ui-draggable {
             background: #ffcb05;
             border: 2px solid black;
             cursor: move;
             width: 200px;
         }
-
         .ui-draggable div {
             padding: 10px;
         }
-
         .ui-draggable div div {
             text-align: center;
         }
-
         .ui-draggable-dragging {
             background: darkgreen;
             border-radius: 3px;
             color: white;
         }
-
         .ui-droppable:not(body) {
             background: #fae7b5;
             border: 6px outset #0f345e;
+            /*display: inline-block;*/
             height: 300px;
             width: 300px;
         }
-
         .ui-droppable.droppedTheMic {
             background: purple;
             color: white;
         }
-
         .ui-droppable p {
             font-weight: bold;
             text-align: center;
@@ -62,15 +74,12 @@
             -ms-user-select: none;
             user-select: none;
         }
-
         .ui-droppable.pickMe {
             background: dodgerblue;
         }
-
         * {
             transition: background 0.5s ease-in-out;
         }
-
         .ui-droppable.almostHaveIt {
             background: orangered;
         }
@@ -79,17 +88,21 @@
 <body>
 <div id="visitor">
     <div>
-        <div>Andrew Brainerd</div>
-        <div>Appointment</div>
-        <div>Linda Kaddouh</div>
+        <div>Visitor: Andrew Brainerd</div>
+        <div>For: Appt w/Linda Kaddouh</div>
     </div>
+    </div>
+<div id="container">
+    <div class="finnaDrop" id="dropItLikeItsHot"></div>
+    <div class="finnaDrop" id="dropItLikeItsHotter"></div>
+    <div class="finnaDrop" id="dropItLikeItsBlazin"></div>
 </div>
-<div class="finnaDrop" id="dropItLikeItsHot"></div>
-<div class="finnaDrop" id="dropItLikeItsHotter"></div>
-<div class="finnaDrop" id="dropItLikeItsBlazin"></div>
+<div id="viewVisitors"></div>
 <? include "jquery.php"; ?>
 <script type="text/javascript">
     $(document).ready(function () {
+        fetchVisitors();
+        $("body").fadeIn();
         var visitor = $("#visitor");
         var dropAreas = $(".finnaDrop");
         visitor.draggable({
@@ -99,34 +112,43 @@
         });
         dropAreas.droppable({
             activate: function (event, ui) {
-                console.log("Activated " + $(this).attr("id"));
                 $(this).addClass("pickMe").html("<p>PICK ME! :D</p>");
             },
             deactivate: function () {
                 $(this).removeClass("pickMe");
             },
             drop: function (event, ui) {
-                console.log("Dropped in the AREA");
                 dropAreas.removeClass("droppedTheMic").empty();
                 var dropID = $(this).attr("id");
-                var status;
+                var status, statusText;
                 switch (dropID) {
                     case "dropItLikeItsHot":
-                        status = "Waiting";
+                        status = 0;
+                        statusText = "Waiting";
                         break;
                     case "dropItLikeItsHotter":
-                        status = "With MSR";
+                        status = 1;
+                        statusText = "With MSR";
                         break;
                     case "dropItLikeItsBlazin":
-                        status = "Done";
+                        status = 2;
+                        statusText = "Done";
                         break;
                     default:
                         console.log("Mucked it up. Nice Job");
                 }
                 $(this)
-                    .html("<p>Changed status to " + status + "</p>")
+                    .html("<p>Changed status to " + statusText + "</p>")
                     .removeClass("almostHaveIt")
                     .addClass("droppedTheMic");
+                $.ajax({
+                    type: "POST",
+                    url: "updateStatus.php",
+                    data: "vid=155&status=" + status,
+                    success: function (msg) {
+                        fetchVisitors();
+                    }
+                });
             },
             greedy: true,
             out: function () {
@@ -138,13 +160,22 @@
         });
         $("body").droppable({
             drop: function (event, ui) {
-                console.log("Dropped on the BODY");
                 $(dropAreas)
                     .removeClass("droppedTheMic")
                     .html("<p>Nothing going on over here</p>");
             }
         });
     });
+    function fetchVisitors() {
+        $.ajax({
+            type: "POST",
+            url: "viewVisitors.php",
+            data: "branch=William",
+            success: function (data) {
+                $("#viewVisitors").html(data);
+            }
+        });
+    }
 </script>
 </body>
 </html>
