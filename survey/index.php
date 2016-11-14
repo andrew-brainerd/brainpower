5b70f69103647566adcb800e08df8ba19a6fb22c
Andrew Brainerd <abrainerd@umcu.org>
2016-11-14 10:55:43 -0500

    Survey start

---
+++ survey/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrew
+ * Date: 11/12/16
+ * Time: 5:43 PM
+ */
+include "dataconnect.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Member Satisfaction Survey</title>
+    <link rel="stylesheet" href="survey.css">
+</head>
+<body>
+    <div id="container">
+        <img src="happy.svg" id="positive" alt="Happy"/>
+        <img src="meh.svg" id="neutral" alt="Meh"/>
+        <img src="sad.svg" id="negative" alt="Sad"/>
+    </div>
+</body>
+</html>
