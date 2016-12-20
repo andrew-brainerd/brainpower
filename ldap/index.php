ab41ebd9ea625937797b6573572ca87aafd40e0c
Andrew Brainerd <abrainerd@umcu.org>
2016-12-20 16:24:44 -0500

    Removed snow. It was annoying.

--- ldap/index.php
+++ ldap/index.php
@@ -1,10 +1,17 @@
+<?php
+$clientIP = $_SERVER['REMOTE_ADDR'];
+if ($clientIP != "198.111.188.194" && $clientIP != "198.0.123.94" && $clientIP != "10.16.2.101") {
+    //header("Location: ");
+    die("<h1>Not Authorized to Access this Resource from " . $clientIP . "</h1>");
+} else {}
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Unlock AD Users</title>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.min.css">
     <link rel="stylesheet" href="ldap.css">
-    <link rel="stylesheet" href="/css/snow.css">
+    <!--<link rel="stylesheet" href="/css/snow.css">-->
 </head>
 <body>
 <div id="snow">
