37ae83ed513fff008cae97ade68fff9489db4c2d
Andrew Brainerd <abrainerd@umcu.org>
2016-12-20 14:04:17 -0500

    Added snow effect to unlocking tool, for funs and holiday spirits

--- ldap/index.php
+++ ldap/index.php
@@ -4,10 +4,13 @@
     <title>Unlock AD Users</title>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.min.css">
     <link rel="stylesheet" href="ldap.css">
+    <link rel="stylesheet" href="/css/snow.css">
 </head>
 <body>
-<div id="message"></div>
-<div id="anonmessage"></div>
+<div id="snow">
+    <div id="message"></div>
+    <div id="anonmessage"></div>
+</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script src="ldap.js"></script>
